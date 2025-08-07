e0cbc4fa55b5a8e9937782c78ecd061c7b8e17d1
Michael Dunn <dunn0562@umn.edu>
2025-08-07 16:08:21 -0500

    Added image resizing

--- ImageLoader.py
+++ ImageLoader.py
@@ -4,8 +4,13 @@ from config import app
 import os
 from werkzeug.utils import secure_filename
 
+from PIL import Image
+
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 DEFAULT_IMAGE_PATH = f"/static/images/default.jpg"
+
+# I'm not sure if 500, 500 is a good quality, but this keeps it kinda not blury
+TARGET_IMAGE_SIZE = (500, 500)
 
 # creates the folder that will contain images, function called immediately by main
 def create_upload_folder():
@@ -24,6 +29,9 @@ def upload_image(recipe_image):
         filename = secure_filename(recipe_image.filename)
         save_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         recipe_image.save(save_path)
+
+        resize_image(save_path) # overwrites the full image, not sure if we want this
+
         image_path = f"/static/images/uploaded/{filename}"
     else:
         image_path = DEFAULT_IMAGE_PATH
@@ -35,5 +43,35 @@ def edit_image(new_image, recipe):
     if new_image and allowed_file(new_image.filename):
         filename = secure_filename(new_image.filename)
         save_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        
         new_image.save(save_path)
+
+        resize_image(save_path) # overwrites the full image, not sure if we want this
+
         recipe.image = f"/static/images/uploaded/{filename}"
+
+def resize_image(image_path):
+    try:
+        img = Image.open(image_path)
+    except FileNotFoundError:
+        print("File not found")
+        return
+    
+    width, height = img.size
+    min_side = min(width, height)
+
+    # Calculate coordinates for a centered crop
+    left = (width - min_side) // 2
+    top = (height - min_side) // 2
+    right = left + min_side
+    bottom = top + min_side
+
+    # Crop the center square
+    img_cropped = img.crop((left, top, right, bottom))
+
+    # Resize the square crop to the desired size
+    img_resized = img_cropped.resize((TARGET_IMAGE_SIZE), Image.LANCZOS)
+
+    img_resized.save(image_path)
+    print(f"Image saved to {image_path}")
+    
